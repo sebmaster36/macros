2ad6da084a01a6c3b9b8ff592bd687b59edf96b0
Sebastian Gutierrez <sgutier5@nd.edu>
2022-01-30 09:33:20 +0000

    completed basic JSON delivery

--- restserver/main.py
+++ restserver/main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with, marshal
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # DB models
@@ -21,10 +22,41 @@ class FoodModel(db.Model):
     crb = db.Column(db.Float, nullable=True)
     sgr = db.Column(db.Float, nullable=True)
 
+    '''
     def __repr__(self):
-        return f'dbno: {fid}'
+        return f'dbno: {self.fid}'
+    '''
 
 # db.create_all() # uncomment if redesigning ORM schema
+
+# Landing Page
+
+@app.route('/')
+def landing():
+    return '<h1>Welcome to the foods API!<h1>'
+
+# API Endpoints & Setup 
+
+resource_fields = { # figure out how to restructure
+    "id":       fields.Integer(attribute='fid'),
+    "name":     fields.String(attribute='nme'),
+    "weight":   fields.Float(attribute='wgt'),      
+    "serving":  fields.String(attribute='srv'),
+    "protein":  fields.Float(attribute='prt'),
+    "fat":      fields.Float(attribute='fat'),
+    "carbs":    fields.Float(attribute='crb'),
+    "sugar":    fields.Float(attribute='sgr')
+}
+
+class FoodsListAPI(Resource):
+    def get(self, crb, fat, pro, sug):
+        print(crb, pro, fat, sug)
+
+
+        result = FoodModel.query.order_by(FoodModel.nme).all()
+        return [ marshal(food, resource_fields) for food in result ]
+        
+api.add_resource(FoodsListAPI, '/api/v1.0/foods/<float:crb>/<float:fat>/<float:pro>/<float:sug>', endpoint='foods')
 
 if __name__ == '__main__':
     app.run(debug=True)
